9d6499786f9f05e29ca7bedaebd95a8205ca3857
Austin Carman <abcarman12@gmail.com>
2021-05-20 15:16:43 -0600

    adding margin-bottom to header

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -9,6 +9,7 @@ const StyleHeader = styled.header `
     align-items: center;
     background-image: url('https://images.unsplash.com/photo-1506703719100-a0f3a48c0f86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80');
     height: 30vh;
+    margin-bottom: 10%;
     div {
         width: 10%;
     }
